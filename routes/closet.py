264fb51bd4b6f6477c446b54613c3d1280f6d366
김민기 <alsrl6710@gmail.com>
2026-08-25 17:22:42 +0900

    블루프린트 제대로 작동되게 수정

---
+++ routes/closet.py
@@ -0,0 +1,3 @@
+from flask import Blueprint, render_template
+
+closet_bp = Blueprint('closet', __name__)
---
+++ routes/mypage.py
@@ -0,0 +1,3 @@
+from flask import Blueprint, render_template
+
+mypage_bp = Blueprint('mypage', __name__)
